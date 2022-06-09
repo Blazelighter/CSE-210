3e0684733a807976867b1e638e94aba636353826
yourusername <wah21001@byui.edu>
2022-06-09 13:17:04 -0600

    properly setting up HelloWorld

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -4,7 +4,8 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");   
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Working."); 
         }
     }
 }
